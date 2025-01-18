31cd4334b4fd5b6d791d5bff414743ea59662e46
derekvmcintire <derekcommonground@gmail.com>
2025-01-18 13:43:16 -0500

    Basic event details component

---
+++ packages/client/src/components/EventDetails/index.tsx
@@ -0,0 +1,56 @@
+import { Container, Divider, Grid, Stack, Text } from "@mantine/core";
+import classes from './event.module.css';
+import type { Event } from "../../types";
+import { formatEventDate } from "../../utils/dates";
+
+type EventProps = {
+  event: Event;
+};
+
+export default function EventDetails({
+  event
+}: EventProps) {
+  const { date, name, city, state, eventUrl } = event;
+
+  // Format the date and split it into weekday and date
+  const formattedDate = formatEventDate(date);
+  const [weekday, dateString] = formattedDate.split(', ');
+
+  return (
+    <Container className={classes.eventContainer}>
+      <Divider />
+      <Grid w="100%" className={classes.eventGrid}>
+        {/* Left column: Date */}
+        <Grid.Col span={4}>
+          <Stack align="flex-start">
+            <Text className={classes.eventDate} style={{ fontWeight: 600, fontSize: '1.2rem' }}>
+              {weekday}
+            </Text>
+            <Text className={classes.eventDate} style={{ fontWeight: 400, fontSize: '1rem' }}>
+              {dateString}
+            </Text>
+          </Stack>
+        </Grid.Col>
+        {/* Right column: Event details */}
+        <Grid.Col span={8}>
+          <Stack align="flex-start">
+            {/* Event Name */}
+            <Text className={`${classes.eventName}`} style={{ fontWeight: 700, fontSize: '1.5rem' }} lineClamp={2}>
+              {name}
+            </Text>
+            {/* Event Location */}
+            <Text className={classes.eventLocation} style={{ fontWeight: 500, fontSize: '1.1rem' }}>
+              {`${city}, ${state}`}
+            </Text>
+            {/* Event URL */}
+            <Text className={classes.eventLink}>
+              <a href={eventUrl} target="_blank" rel="noopener noreferrer" style={{ fontWeight: 500, color: '#1e90ff', fontSize: '1.1rem' }}>
+                Link to Reg
+              </a>
+            </Text>
+          </Stack>
+        </Grid.Col>
+      </Grid>
+    </Container>
+  );
+}
---
+++ packages/client/src/components/TopNav/index.tsx
@@ -0,0 +1,31 @@
+import { Flex, Group, Text, Title } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+import HamburgerNav from './HamburgerNav';
+import NavLinks from './NavLinks';
+import classes from './top-nav.module.css';
+
+export const TOP_NAV_TEST_ID = 'top-nav';
+
+export default function TopNav() {
+  const isMobile = useMediaQuery('(max-width: 950px)');
+
+  return (
+    <div data-testid={TOP_NAV_TEST_ID} className={classes.topNavContainer}>
+      <Flex justify="space-between" align="center" pt={10}>
+        {isMobile ? <HamburgerNav /> : <NavLinks />}
+        <Group>
+          <Title className={classes.title} ta="right">
+            <Text
+              inherit
+              variant="gradient"
+              component="span"
+              gradient={{ from: "#f0b343", to: "#4385f0" }}
+            >
+              B2C2 Event Calendar
+            </Text>
+          </Title>
+        </Group>
+      </Flex>
+    </div>
+  );
+}
