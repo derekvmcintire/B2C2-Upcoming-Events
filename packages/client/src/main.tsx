1efd5a9d5ea17d8e26c96147c7a4e5fb5044e7ac
derekvmcintire <derekcommonground@gmail.com>
2025-01-17 20:44:19 -0500

    Make shit normal again

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,17 @@
+<<<<<<< HEAD:packages/client/src/main.tsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+=======
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+>>>>>>> parent of 9fa89b6 (Add prettier and scripts):apps/client/src/main.tsx
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
+)
