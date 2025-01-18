06b3dbb73434046c7f38fb751ab5695351aaf72c
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2025-01-18 15:32:10 -0500

    Fetch registered riders (#22)
    * Write basic function
    
    * dont use button

--- packages/client/src/components/TopNav/NavLinks.tsx
+++ packages/client/src/components/TopNav/NavLinks.tsx
@@ -1,22 +1,9 @@
-import { Anchor, Button, Divider, Flex, Stack } from '@mantine/core';
+import { Anchor, Divider, Flex, Stack } from '@mantine/core';
 import classes from './top-nav.module.css';
 
 interface NavLinksProps {
   isVertical?: boolean;
 }
-
-const getNavButton = (text: string) => {
-  return (
-    <Button
-      c="#FFF"
-      size="md"
-      variant="subtle"
-      justify="space-between"
-      leftSection={text}
-      rightSection={<span />}
-    />
-  );
-};
 
 const getNavLinks = ({ isVertical = false }: NavLinksProps) => {
   // Divider orientation is opposite of nav
@@ -24,19 +11,19 @@ const getNavLinks = ({ isVertical = false }: NavLinksProps) => {
   return (
     <>
       <Anchor underline="never" className={classes.topNavAnchor} href="/">
-        {getNavButton('Home')}
+        Home
       </Anchor>
       <Divider ml={8} mr={8} orientation={dividerOrientation} />
       <Anchor underline="never" className={classes.topNavAnchor} href="/road">
-        {getNavButton('Road')}
+        Road
       </Anchor>
       <Divider ml={8} mr={8} size="sm" orientation={dividerOrientation} />
       <Anchor underline="never" className={classes.topNavAnchor} href="/cx">
-        {getNavButton('CX')}
+        CX
       </Anchor>
       <Divider ml={8} mr={8} size="sm" orientation={dividerOrientation} />
       <Anchor underline="never" className={classes.topNavAnchor} href="/xc">
-        {getNavButton('XC')}
+        XC
       </Anchor>
     </>
   );
