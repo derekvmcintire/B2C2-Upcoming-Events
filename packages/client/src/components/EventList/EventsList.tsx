f3fc0b65cba9a3f18b26fb29835aab251353f400
derekvmcintire <derekcommonground@gmail.com>
2025-01-19 13:40:07 -0500

    remove commented out code

--- packages/client/src/components/EventList/EventsList.tsx
+++ packages/client/src/components/EventList/EventsList.tsx
@@ -16,26 +16,6 @@ export default function EventsList({ discipline }: EventsListProps): JSX.Element
     const eventsContext = useEventsContext();
     const { events, registrations } = eventsContext;
 
-//   // Fetch registrations on component mount
-//   useEffect(() => {
-//     const getRegisteredRiders = async () => {
-//       const response = await fetchRegistrations(discipline.queryParam);
-//       setRegistrations(response);
-//     };
-
-//     getRegisteredRiders();
-//   }, [setRegistrations]);
-
-//   // Fetch categorized events on component mount
-//   useEffect(() => {
-//     const getEvents = async () => {
-//       const response = await fetchEventsByType(discipline.id);
-//       setEvents(response.events);
-//     };
-
-//     getEvents();
-//   }, [setEvents]);
-
     return events.length < 1 ? (
         <Text mt="16">{`No ${discipline.text} events found.`}</Text>
     ) : (
