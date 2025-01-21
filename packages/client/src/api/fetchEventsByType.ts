43ea541b4a8ae3ceb7272b36ff0dd2d036a0f576
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2025-01-21 16:46:29 -0500

    Add api token header (#50)
    * add api secret key
    
    * Add api secret key

--- packages/client/src/api/fetchEventsByType.ts
+++ packages/client/src/api/fetchEventsByType.ts
@@ -3,9 +3,12 @@ import { GetEventsResponse } from "../types";
 
 const url = 'https://b2c2-events-api.vercel.app/api/getEventsByType';
 
+
 export const fetchEventsByType = async (type: string): Promise<GetEventsResponse> => {
+  const apiKey = import.meta.env.API_SECRET_KEY;
   const response = await simple(url)
     .params({type: type})
+    .headers({"x-api-key": apiKey})
     .fetch<GetEventsResponse>();
 
   return response.data;
