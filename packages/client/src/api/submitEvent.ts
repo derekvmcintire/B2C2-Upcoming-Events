5ecd01599d0fe81974c6e9bf43b40808ab852c09
derekvmcintire <derekcommonground@gmail.com>
2025-01-21 16:44:35 -0500

    add api secret key

--- packages/client/src/api/submitEvent.ts
+++ packages/client/src/api/submitEvent.ts
@@ -4,8 +4,10 @@ import { EventSubmission } from "../types";
 const url = 'https://b2c2-events-api.vercel.app/api/submitEvent';
 
 export const submitEvent = async (submission: EventSubmission) => {
+  const apiKey = import.meta.env.API_SECRET_KEY;
   const response = await simple(url)
     .body<EventSubmission>(submission)
+    .headers({"x-api-key": apiKey})
     .post();
 
     console.log('response is: ', response);
