ae8d33f25a14d4a14b529d08bd998237343a2799
derekvmcintire <derekcommonground@gmail.com>
2025-01-29 08:34:17 -0500

    Check for loading not expandable table

--- packages/client/cypress/e2e/home/home.cy.ts
+++ packages/client/cypress/e2e/home/home.cy.ts
@@ -7,7 +7,7 @@ describe("Homepage", () => {
     cy.contains("B2C2 Event Calendar").should("be.visible");
     cy.get('[data-testid="nav"]').should("exist");
     cy.get('[data-testid="sub-drawer-button"]').should("exist");
-    cy.get('[data-testid="expandable-table"]').should("exist");
+    cy.get('[data-testid="loading"]').should("exist");
   });
 
   it("should navigate to a specific page", () => {
